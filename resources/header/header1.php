8ca85a02c07d8695acfd089b0942185156220ea4
gartmannsa <gartmann.sandro@bluewin.ch>
2021-07-02 17:08:29 +0200

    nav bar

---
+++ resources/header/header1.php
@@ -0,0 +1,15 @@
+
+<nav>
+    <ul>
+        <a href="./" ><img src="./src/img/logo_easy.png" id="imglogo"/></a>
+        <li>
+            <a href="./noel">Home</a>
+        </li>
+        <li>
+            <a href="./noel">Memes</a>
+        </li>
+        <li>
+            <a href="">Create New</a>
+        </li>
+    </ul>
+</nav>
