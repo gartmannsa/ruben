93a9d6ae884347a6746d2bf1c5cc2a94ff1bb65a
gartmannsa <gartmann.sandro@bluewin.ch>
2021-07-07 14:37:40 +0200

    login action

--- resources/body/login.php
+++ resources/body/login.php
@@ -4,7 +4,7 @@
 <div id="loginpanel">
     <h1>Login</h1>
     <div id="loginform">
-        <form class="custom_form" method="post" action="">
+        <form class="custom_form" method="post" action="../actions/loginaction.php">
             <input type="text" id="username_login" name="username_login" placeholder="Benutzername"><br>
             <input type="password" id="pw_login" name="pw_login" placeholder="Passwort"><br>
             <button type="submit" id="btn_submit_login" name="btn_submit_login">Anmelden</button>
